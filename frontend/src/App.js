779459626c7b8c1d094a319c9e79d3bd1b9070f9
Suman Bej <sumanbej.glt@gmail.com>
2024-05-31 23:47:08 +0530

    added frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
+
+export default App;
