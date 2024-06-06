cfd49e7642828b41641caadb7eb70ab887acbd05
Suman Bej <sumanbej.glt@gmail.com>
2024-06-06 22:14:16 +0530

    chat and mesage backend added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,23 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
+//Get or Search all users
+//@route           GET /api/user?search=
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
 //Register New User
 //path-- (POST----/api/user)
 const registerUser = asyncHandler(async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
@@ -10,10 +12,12 @@ app.use(express.json());
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is working");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is working");
+// });
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 5008;
 app.listen(PORT, console.log(`Server running at ${PORT}`));
